e268faae95468b071678da117200d9871837face
Aaron Currie <baron_currie@hotmail.com>
2026-04-14 08:26:59 +0100

    cats/acts own functions

--- src/components/editing/pill-editing/pill-editor.tsx
+++ src/components/editing/pill-editing/pill-editor.tsx
@@ -2,10 +2,9 @@ import styles from "./pill-editor.module.css"
 import Model from "@/components/model/model"
 import { Overlay } from "@/components/utility-comps/overlay"
 import { Activity, Category } from "@/lib/types/goals"
-import { useEffect, useState } from "react"
+import { useState } from "react"
 import ItemEditor from "../item-editor/item-editor"
 import AddNewItem from "../add-new-item/add-new-item"
-import { addItem } from "@/lib/db-calls/item/add-item"
 import ErrorModal from "@/components/error/error-modal/error-modal"
 
 type PillEditingProps = {
@@ -16,46 +15,16 @@ type PillEditingProps = {
 }
 
 export default function PillEditor({ group, label, setEditing, setGroupState }: PillEditingProps) {
-    const [loading, setLoading] = useState(false);
     const [error, setError] = useState<string | null>(null);
-    const [validation, setValidation] = useState<{[key: string]: string}>({});
-
-    const [newItem, setNewItem] = useState<string>("");
-    
-    const handleAdd = async () => {
-        if(!newItem) {
-            setValidation({ ...validation, newItem: "This field is required" });
-            return;
-        }
-        setLoading(true);
-        let item: Category | Activity;
-        try {
-            item = await addItem({ label, newItem });
-
-        } catch (error: any) {
-            setError(error.message);
-            setLoading(false);
-            return;
-        }
-        setNewItem("");
-        setGroupState((prev) => [...prev, item]);
-        setLoading(false);
-    }
-
-    useEffect(() => {
-        if(newItem) {
-            setValidation({ ...validation, newItem: "" });
-        }
-    }, [newItem])
 
     return (
         <Overlay onClick={() => setEditing(false)}>
             <Model onClose={() => setEditing(false)}>
                 <div className={styles.container}>
-                    {loading ? <p>Loading...</p> : <AddNewItem validation={validation.newItem} label={label} newItem={newItem} setNewItem={setNewItem} handleAdd={handleAdd} />}
+                    <AddNewItem label={label} setError={setError} setGroupState={setGroupState} />
                     {group.map((item) => {
                         return (
-                            <ItemEditor key={item.id} item={item} label={label} setGroupState={setGroupState} />
+                            <ItemEditor key={item.id} item={item} label={label} setError={setError} setGroupState={setGroupState} />
                         )
                     })}
                 </div>
